5f1e8c76f44d72d3425bf483d98e54e50a00d59a
dredivaris <dredivaris@gmail.com>
2015-12-11 22:35:29 -0500

    added setup.py to allow for pip installations

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 linkedin-selenium-scraper 0.1.0
 ===============================
 
+I created this scraper to help me pull my Linkedin profile data for a live resume.  Anything 
+available on a LinkedIn public profile should be accessible through this interface.
 
 
 Quickstart
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='linkedin_selenium_scraper',
+      version='0.1.0',
+      description='A python 3.5 compatible linkedin scraper utilizing selenium',
+      author='Andreas Divaris',
+      author_email='andreas.divaris@gmail.com',
+      url='https://github.com/dredivaris/linkedin_selenium_scraper',
+      install_requires=['selenium>=2.48'],
+     )
